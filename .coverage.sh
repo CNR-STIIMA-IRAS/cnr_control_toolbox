c2191dd2e1a104e412b01595bb0a6a2daa032892
Nicola Pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2022-03-08 10:48:38 +0100

    code coverage fix

--- .coverage.sh
+++ .coverage.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
 
-
-echo "Generating coverage for 'cnr_control_toolbox'"
+declare -a StringArray=("state_space_systems" "state_space_controllers" "state_space_filters" "state_space_ros")
 
 ws=~/target_ws
+
+cp /home/runner/work/cnr_control_toolbox/cnr_control_toolbox/codecov.yml "$ws"/
+
 cd "$ws"
-catkin build state_space_systems     -v --no-deps --catkin-make-args coverage_report  --cmake-args -DENABLE_COVERAGE_TESTING=ON -DCMAKE_BUILD_TYPE=Debug -DUSE_ROS=ON -DENABLE_TESTING=ON
-catkin build state_space_controllers -v --no-deps --catkin-make-args coverage_report  --cmake-args -DENABLE_COVERAGE_TESTING=ON -DCMAKE_BUILD_TYPE=Debug -DUSE_ROS=ON -DENABLE_TESTING=ON
-catkin build state_space_filters     -v --no-deps --catkin-make-args coverage_report  --cmake-args -DENABLE_COVERAGE_TESTING=ON -DCMAKE_BUILD_TYPE=Debug -DUSE_ROS=ON -DENABLE_TESTING=ON
-catkin build state_space_ros         -v --no-deps --catkin-make-args coverage_report  --cmake-args -DENABLE_COVERAGE_TESTING=ON -DCMAKE_BUILD_TYPE=Debug -DUSE_ROS=ON -DENABLE_TESTING=ON
 
-#echo "Uploading coverage results to codecov.io"
+for val in ${StringArray[@]}; do
 
-# Remove duplicated information
-rm "$ws/build/state_space_systems/coverage_report.info.cleaned"
-rm "$ws/build/state_space_systems/coverage_report.info.removed"
+    echo "Generating coverage for '$val'"
 
-rm "$ws/build/state_space_controllers/coverage_report.info.cleaned"
-rm "$ws/build/state_space_controllers/coverage_report.info.removed"
+    catkin build "$val" --no-deps --catkin-make-args run_tests 
+    catkin build "$val" --no-deps --catkin-make-args coverage_report --cmake-args -DENABLE_COVERAGE_TESTING=ON -DCMAKE_BUILD_TYPE=Debug -DCATKIN_ENABLE_TESTING=ON
 
-rm "$ws/build/state_space_filters/coverage_report.info.cleaned"
-rm "$ws/build/state_space_filters/coverage_report.info.removed"
-
-rm "$ws/build/state_space_ros/coverage_report.info.cleaned"
-rm "$ws/build/state_space_ros/coverage_report.info.removed"
-
-# # Actually upload coverage informationstate_space_controllers
-# bash <(curl -s https://codecov.io/bash) -s "$ws/build/state_space_systems/"
-# bash <(curl -s https://codecov.io/bash) -s "$ws/build/state_space_controllers/"
-# bash <(curl -s https://codecov.io/bash) -s "$ws/build/state_space_filters/"
-# bash <(curl -s https://codecov.io/bash) -s "$ws/build/state_space_ros/"
+    # Remove duplicated information
+    rm "$ws/build/$val/coverage_report.info.cleaned"
+    rm "$ws/build/$val/coverage_report.info.removed"
+done
