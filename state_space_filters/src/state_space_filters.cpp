5db88acccbdbc82cbe05ce807ae74e2ddf008dc1
Nicola Pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2023-03-24 16:18:16 +0100

    removed ros from state_space_filters

---
+++ state_space_filters/src/state_space_filters.cpp
@@ -0,0 +1,12 @@
+
+#include <state_space_filters/common_filters.h>
+#include <state_space_filters/filtered_values.h>
+#include <state_space_filters/fir_filters.h>
+#include <state_space_filters/iir_filters.h>
+#include <state_space_filters/sg_filter.h>
+
+
+namespace eigen_control_toolbox
+{
+  // notghing to do
+}
